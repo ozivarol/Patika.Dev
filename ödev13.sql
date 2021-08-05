5a9313a48b1bf660d3a96d019a36db0efbfb1055
ozivarol <85025618+ozivarol@users.noreply.github.com>
2021-08-05 20:40:02 +0300

    Update ödev13.sql

--- ödev13.sql
+++ ödev13.sql
@@ -6,3 +6,12 @@ WHERE page_number >(
 
 )
 ORDER BY page_number DESC
+
+Select film.title,film.rental_rate,film.replacement_cost ,(SELECT (AVG(film.replacement_cost)) from film),( film.replacement_cost- (Select Avg(film.replacement_cost) from film) )as differ from film
+WHERE replacement_cost >(
+    Select replacement_cost from film
+	WHERE title='Airport Pollock'
+
+)
+Order by replacement_cost DESC
+
